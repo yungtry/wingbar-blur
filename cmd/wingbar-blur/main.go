c0dd0d6877879f86f040c55b160e42e2d0efde83
yungtry <yungtry@users.noreply.github.com>
2020-03-08 13:17:00 +0100

    size as int

--- cmd/wingbar-blur/main.go
+++ cmd/wingbar-blur/main.go
@@ -12,8 +12,8 @@ import (
 var opts struct {
 	Path string `long:"path" description:"Path to the wallpaper." required:"true"`
 	Theme string `short:"t" long:"theme" description:"Choose theme: none, light, dark." default:"none"`
-	Blur float64 `short:"b" long:"blur" description:"Choose blur intensity." default:"10"`
-	Size string `short:"s" long:"size" description:"Choose size: small, normal, big" default:"normal"`
+	Blur float64 `short:"b" long:"blur" description:"Choose blur intensity [number]" default:"10"`
+	Size int `short:"s" long:"size" description:"Choose size [number]" default:"20"`
 	Change bool `short:"c" long:"change" description:"Set the output as wallpaper."`
 }
 
@@ -28,17 +28,17 @@ func main() {
 	Process(src, opts.Theme, opts.Blur, opts.Size, opts.Change)
 }
 
-func Process(src image.Image, theme string, blur float64, size string, change bool) {
+func Process(src image.Image, theme string, blur float64, size int, change bool) {
 	img1 := imaging.Blur(src, blur)
-	img2 := imaging.CropAnchor(img1, src.Bounds().Dx(), TranslateSize(size), imaging.TopLeft)
+	img2 := imaging.CropAnchor(img1, src.Bounds().Dx(), size, imaging.TopLeft)
 
-	overlay := imaging.New(src.Bounds().Dx(), TranslateSize(size), TranslateColor(theme))
+	overlay := imaging.New(src.Bounds().Dx(), size, TranslateColor(theme))
 	img3 := imaging.Overlay(img2, overlay, image.Pt(0, 0), 0.4)
 
 	dst := imaging.New(src.Bounds().Dx(), src.Bounds().Dy(), color.NRGBA{0, 0, 0, 0})
 	dst = imaging.Paste(dst, src, image.Pt(0, 0))
 	dst = imaging.Paste(dst, img3, image.Pt(0, 0))
-	err := imaging.Save(dst, "blur.jpg")
+	err := imaging.Save(dst, "blur.png")
 	if err != nil {
 		log.Fatalf("failed to save image: %v", err)
 	}
